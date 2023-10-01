67544ee1b7f4a62eb189f39cdafeb33842d5414b
Arizidane <samionohari@gmail.com>
2023-10-01 20:31:58 +0700

    chore: Create new function init

--- js/fetch.js
+++ js/fetch.js
@@ -18,7 +18,7 @@ fetch(url)
 	.then(response => response.json())
 	.then(data => {
 		// document.getElementById('artwork-image').src = data.artwork
-		
+				
 		for (let i = 0; i < data.length; i++) {
 			// console.log(data[i].title);
 			const format = `
@@ -67,5 +67,55 @@ async function fetchById(id) {
 		console.error(msg);
 		msgPopup(msg)
     	throw e;
+	}
+}
+
+// POST Method
+async function createNew(event) {
+	event.preventDefault()
+
+	let lastID = 0
+	fetch(url).then(data => {
+		return lastID = Math.max(...data.map(item => item.id));
+	})
+	console.log(lastID);
+
+	const title = document.getElementById('title').value
+	const artist = document.getElementById('artist').value
+	const album = document.getElementById('album').value
+	const year = document.getElementById('year').value
+	const genre = document.getElementById('genre').value
+	const art = document.getElementById('artwork').files[0]
+	const song = document.getElementById('song').files[0]
+	const lyrics = document.getElementById('lyrics').value
+
+	const formData = new FormData();
+    formData.append('title', title);
+    formData.append('artist', artist);
+    formData.append('album', album);
+    formData.append('year', year);
+    formData.append('genre', genre);
+    formData.append('artwork', artwork);
+    formData.append('song', song);
+    formData.append('lyrics', lyrics);
+    formData.append('id', lastID++);
+
+	try {
+		const response = await fetch(url, {
+			method: 'POST',
+			body: formData
+		})
+
+		if (response.ok) {
+			console.log('Sukses tambah data');
+			msgPopup('Sukses tambah data')
+		} else {
+			console.error('Gagal tambah data');
+			msgPopup('Gagal tambah data')
+		}
+	} catch (error) {
+		const msg = `Permintaan POST error: ${error}`
+		console.error(msg)
+		msgPopup(msg)
 	}
 }
